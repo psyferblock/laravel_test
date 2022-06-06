b090a69fad652918a9a51234a9933f694c47da38
psyfer <psyferblock.io@gmail.com>
2022-06-06 17:39:07 +0300

    time api

---
+++ app/Http/Controllers/Palindrome.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Palindrome extends Controller
+{
+    //
+    public function PalindromeCount(){
+        $words = ["text","spike","tom","jerry"];
+       echo $words[3];
+    }
+}
---
+++ app/Http/Controllers/Time.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Time extends Controller
+{
+    //
+    
+    public function GetTime (){
+        $time=strtotime('14-04-1732');
+        $current_time=new date();
+
+        $difference=$current_time - $time;
+        echo $difference
+    }
+}
+
+
